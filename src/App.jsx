a9cbd1e9ba74aa8169c746234b24ab47de025c4e
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2024-12-14 01:14:11 -0300

    PTF-2 - Estilizando formulário de endereço e renomeando arquivo

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { Home } from './View/Home'
-import { Button } from './Components/Button'
+import { Address } from './View/Address'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  function contar() {
-    return setCount(count+1);
-  }
-
   return (
     <>
-      <Home count={count}/>
-      {/* <Button contar={contar}/> */}
+      <Address />
     </>
   )
 }
