c37dd688d05911ac7a01353237c00bd88ab74b43
Gabriel Ribeiro <gabrielribeiroonline@gmail.com>
2024-12-14 02:09:43 -0300

    PTF-2 - Sistema de rotas funcionando

--- src/main.jsx
+++ src/main.jsx
@@ -25,7 +25,6 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
     <RouterProvider router={router} />
   </StrictMode>,
 )
